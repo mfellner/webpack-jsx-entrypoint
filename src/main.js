969c26cc4a7dde0dd1ca27ad194fc13cabe6efcf
Maximilian Fellner <max.fellner@gmail.com>
2015-08-20 00:13:02 +0200

    Initialize gh-pages

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import content from './content.jsx'
-
-export function init() {
-  const Content = React.createFactory(content)
-  React.render(Content(), document.getElementById('main'));
-}
